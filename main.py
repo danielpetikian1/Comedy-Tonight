05f89e70b7c050701d2d33fbf0d79420139b6674
masonLighthouse <mason@thelighthouseducationproject.com>
2021-05-03 11:38:22 -0500

    defining main.py to be the main function. agent_1 and 2 are scriptDB class instances for now, as we do not have anything specific for any agent (they are the same and have no discerning properties. Next step is to make some methods that generate sentences using the script DB

---
+++ main.py
@@ -0,0 +1,12 @@
+from script_db import ScriptDB
+
+def main():
+    agent_1 = ScriptDB('./data/script_db.json')
+    agent_2 = ScriptDB('./data/script_db.json')
+
+    # for 10 back in forth messages
+    for i in range(0, 10):
+        print("hi!")
+
+if __name__ == "__main__":
+    main()
