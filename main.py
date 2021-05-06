93cec3e9a69209bca9fc01dcde485c356b47e7b2
masonLighthouse <mason@thelighthouseducationproject.com>
2021-05-06 11:26:28 -0500

    added _ to main loop

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     agent_2 = ScriptDB("./data/script_db.json")
     print("Comedy Tonight Script Generator v0.1.0: \n\n")
     # for 10 back in forth messages
-    for i in range(0, 10):
+    for _ in range(0, 10):
         print(
             f"{agent_1.generate_what_sentence()} - {agent_1.generate_why_sentence()} \n"
         )
