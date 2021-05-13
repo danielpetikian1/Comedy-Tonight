a8058e218c1dec510fdebd30726b83c1afced99b
masonLighthouse <mason@thelighthouseducationproject.com>
2021-05-13 14:54:17 -0500

    making a new agent class

---
+++ agent.py
@@ -0,0 +1,19 @@
+import random
+from testAPI import (
+    get_capable,
+    get_types_from_animal,
+    get_animals_from_type,
+    get_related_to,
+)
+
+
+class Agent:
+    # init
+    def __init__(self) -> None:
+        pass
+
+    def generate_what_sentence():
+        raise NotImplementedError()
+
+    def generate_why_sentence():
+        raise NotImplementedError()
