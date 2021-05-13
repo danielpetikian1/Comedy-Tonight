7a7290e776c221338e66f42395205e3e67c56507
masonLighthouse <mason@thelighthouseducationproject.com>
2021-05-13 14:48:14 -0500

    working on some more API functions

--- testAPI.py
+++ testAPI.py
@@ -6,7 +6,13 @@ def main():
     type = "lepidopterous_insect"
     print(get_capable(animal))
     print(get_types_from_animal(animal))
+    # gives us verbose list of types of this animal
     print(get_animals_from_type(type))
+    # gives us list of new animals
+    print(get_related_to(get_animals_from_type(type)[0]))
+    # from here can see if something inside of get_related_to(get_animals_from_type(type)[0]) has a 
+    # get capable array, and if not, we need to get a new one. If none have it, maybe do a switch to
+    # something brand new
 
 
 def get_capable(animal: str):
@@ -48,6 +54,18 @@ def get_animals_from_type(type: str):
     )
     obj = response.json()
     animals = [edge["end"]["label"] for edge in obj["edges"]]
+    return animals
+
+def get_related_to(arg: str):
+    """
+    Given some animal, return animals that are related
+    """
+    lower_arg = arg.lower()
+    response = requests.get(
+        f"http://api.conceptnet.io/query?end=/c/en/{lower_arg}&rel=/r/IsA&limit=1000"
+    )
+    obj = response.json()
+    animals = [edge["start"]["label"] for edge in obj["edges"]]
     return animals
 
 
